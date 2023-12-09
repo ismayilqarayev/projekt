9f3c58108fc81b084e90d6d471a6658da7e61aa6
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2023-12-09 22:03:07 +0400

    Create test_1.cpp

---
+++ test_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+using std::endl;
+
+int main()
+{
+	int number_1 = 100;
+	int number_2 = 200;
+
+	cout << "number_1 = " << number_1 << endl;
+	cout << "number_2 = " << number_2 << endl;
+}
