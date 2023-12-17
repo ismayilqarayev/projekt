4df0a7ba6baac70344c9957d5f7a9fd4c0084e28
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2023-12-17 14:03:49 +0400

    Create test_6.cpp

---
+++ test_6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::endl;
+using std::cout;
+using std::cin;
+
+int main()
+{
+	int number_1 = 10;
+	int number_2 = 20;                                                    
+	int a = number_1 + number_2;
+	cout << "number_1 + number_2 = " << a << endl;
+
+
+
+	int number_3 = 30;
+	int number_4 = 40;
+	int b = number_3 + number_4;
+	cout << "number_3 + number_4 = " << b << endl;
+}
