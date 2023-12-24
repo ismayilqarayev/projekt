96d2904d7913ab74cdadea5971e146a9471d1b93
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2023-12-24 16:48:29 +0400

    Create test_4.cpp

---
+++ test_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int num1, num2;
+
+    std::cout << "Birinci ədədi daxil edin: ";
+    std::cin >> num1;
+
+
+    std::cout << "Birinci ededi daxil edin: ";
+    std::cin >> num2;
+
+
+    int sum = num1 + num2;
+    std::cout << "cavab: " << sum << std::endl;
+
+    return 0;
+}
