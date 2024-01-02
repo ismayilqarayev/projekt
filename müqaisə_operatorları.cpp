318a3f85a216ce2cc8fb1e1deabee61da22fa2e3
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2024-01-02 21:20:21 +0400

    Update müqaisə_operatorları.cpp

--- müqaisə_operatorları.cpp
+++ müqaisə_operatorları.cpp
@@ -7,7 +7,7 @@ int main()
     int a = 10;
     int b = 5;
 
-    if (a > b) // a dəyişən b dəyişənə bərabərdirsə onda a və b - nin toplanması
+    if (a > b)
     {
         int c = a + b;
         cout << c;
