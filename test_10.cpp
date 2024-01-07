6610e453a4a9141d248de7857b30e79618974562
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2024-01-07 15:17:10 +0400

    Create test_10.cpp

---
+++ test_10.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int number_1;
+int number_2;
+int number_3;
+int number_4;
+int number_5;
+
+int a_1;
+int b_2;
+int c_3;
+int d_4;
+
+void fun_1()
+{
+	number_1 = 12;
+	number_2 = 45;
+	number_3 = 56;
+	number_4 = 67;
+	number_5 = 9;
+
+	a_1 = 1;
+	b_2 = 2;
+	c_3 = 3;
+	d_4 = 4;
+
+	if (number_1 < number_2)
+	{
+		int get = ((number_3 + number_4) * number_1 * number_2);
+		cout << get << "\n";
+		
+		if (number_4 > d_4)
+		{
+			cout << 
+		}
+	}
+}
+
+int main() 
+{
+	fun_1();
+}
