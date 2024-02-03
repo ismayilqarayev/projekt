b4a9c46013d6345dd747d8d3cc2952a54d832f92
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2024-02-03 20:55:39 +0400

    Create code_16.cpp

---
+++ code_16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int a, n;
+
+    // İstifadəçidən a və n ədədlərini almaq 
+    std::cin >> a;
+    std::cin >> n;
+
+    // a^n qiymətini hesablamaq
+    int result = std::pow(a, n); // a -ya 5 versək n də 2 versək 5 
+                                 // 5 iki defe öz özünə vuracaq yəni 5 * 5 
+    // Nəticəni çap etmək        // bu pow emeliyyatı ilə hesablamadır
+    std::cout << result << std::endl;
+
+    return 0;
+}
