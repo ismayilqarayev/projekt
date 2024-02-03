440b3a4a4d5f8471db4ce08ed93bf83e53057a32
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2024-02-03 20:40:30 +0400

    Create code_13.cpp

---
+++ code_13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    int number_1 = 10;
+    int number_2 = 10;
+
+    while (true) 
+    { 
+        if (number_1 == number_2)
+        {
+            int b = 2 * (number_1 + number_2);
+            cout << b;
+            if (b == 40)
+            {
+                cout << b / 2;
+            }
+            return 0; // return kodunu silsek eger kod tekrarlanacaq
+        }
+
+    }
+}
